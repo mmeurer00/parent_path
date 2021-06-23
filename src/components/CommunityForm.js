3a7c24b269ef3b0080b98e5c2109b6f66ba4e942
Maxine <maxinemeurer@yahoo.com>
2021-06-23 12:48:08 -0400

    create handle change function for communityform

--- src/components/CommunityForm.js
+++ src/components/CommunityForm.js
@@ -1,24 +1,44 @@
 import React from 'react';
 
-const CommunityForm = () => {
-    return (
-      <form>
-        <h2>Make a post!</h2>
-        <div>
-          <input type="text" name="title" placeholder="Title" />
-          <label htmlFor="title">Title</label>
-        </div>
-        <div>
-          <input type="tag" name="tag" placeholder="tag" />
-          <label htmlFor="tag">Tag</label>
-        </div>
-        <div>
-          <input type="content" name="content" placeholder="content" />
-          <label htmlFor="content">Content</label>
-        </div>
-        <input type="submit" value="Create Post" />
-      </form>
-    );
+class CommunityForm extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            formInput: {
+                title: "",
+                content: "",
+                tag: "",
+                location: "",
+                user_id: 1    }
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render(){
+        return (
+        <form >
+            <h2>Make a post!</h2>
+            <div>
+                <input type="text" name="title" placeholder="Title" onChange={this.handleChange} />
+                <label htmlFor="title">Title</label>
+            </div>
+            <div>
+                <input type="tag" name="tag" placeholder="tag" onChange={this.handleChange} />
+                <label htmlFor="tag">Tag</label>
+            </div>
+            <div>
+            <input type="content" name="content" placeholder="content" onChange={this.handleChange}/>
+            <label htmlFor="content">Content</label>
+            </div>
+            <input type="submit" value="Create Post" />
+        </form>
+        );
+    }
 }
 
 export default CommunityForm; 
