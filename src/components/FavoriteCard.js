35c45c5bf9571cc6a8e0cb8069e860684539af75
Maxine <maxinemeurer@yahoo.com>
2021-07-04 09:58:49 -0400

    favorite card styling

--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -1,6 +1,37 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../redux/postSlice'
+import styled from 'styled-components'
+
+const CardContainer = styled.div`
+    position: 'center';
+    width:'100%';
+    height:'100%';
+    margin: 3rem;
+    align-items: 'center';
+    justify-items: 'center';
+`
+const Card = styled.div`
+    margin: auto;
+    postion: absolute;
+    width: 30rem;
+    height: 15rem;
+    padding: auto;
+    text-align: center;
+    background: #ffffff;
+    border-radius: 10px;
+    padding: 2rem;
+    box-shadow: 25px 25px 50px grey, -25px -25px 50px grey
+  `
+const CardContent = styled.div`
+    postion: absolute;
+    margin: auto;
+`
+const Link = styled.p`
+    margin: .5rem;
+    font-size: 1.5 rem
+`
+
 class FavoriteCard extends React.Component {
 
     constructor(props) {
@@ -21,19 +52,23 @@ class FavoriteCard extends React.Component {
             if(post.id == this.props.favorite.post_id)
                 return favPost = {
                     title: post.title, 
-                    content: post.content
+                    content: post.content,
+                    link: post.link
                 }
         })
             
         return (
-            <div className="card">
-                <div className="card__content">
-                    <div className="card__header"></div>
-                        <h1>{this.props.favorite.id}</h1>
-                        <p>{favPost.title}</p>
-                        <p>{favPost.content}</p>
-                </div>
-            </div>
+            <CardContainer>
+                <Card>
+                    <CardContent>
+                        <div className="card__header">{favPost.title}</div>
+                        <div className="card__info">
+                            <p>{favPost.content}</p>
+                            <Link className="link">{favPost.link}</Link>
+                        </div>
+                    </CardContent>
+                </Card>
+            </CardContainer>
         )
     }
 }
